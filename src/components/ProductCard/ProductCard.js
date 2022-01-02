49e09331ce58bf20b7c2e726e26d823802e5d494
Hoàn Nguyễn <softteam2021@gmail.com>
2022-01-03 00:18:47 +0700

    [update] homepage and [add] productcard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,15 +1,31 @@
 import React from "react";
-import "./ProductCard.js";
+import "./ProductCard.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export const ProductCard = (props) => {
   return (
     <div className="productCard">
       <div className="product-img">
-        <img src={props.productImg} />
+        <img className="prod-img" src={props.productImg} />
       </div>
       <div className="product-detail">
-        <span className="product-name">{props.productName}</span>
-        <span className="product-price">{props.productPrice}</span>
+        <p className="prod-name">{props.productName}</p>
+        <div className="prod-price">{props.productPrice}</div>
+      </div>
+      <div className="interact-btn">
+        <div className="add tooltip">
+          <button className="btn">
+            <ShoppingCartIcon />
+          </button>
+          <span class="tooltiptext">Add To Cart</span>
+        </div>
+        <div className="add tooltip">
+          <button className="btn">
+            <FavoriteIcon />
+          </button>
+          <span class="tooltiptext">Add To Wishlist</span>
+        </div>
       </div>
     </div>
   );
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -3,6 +3,7 @@ import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { BestSeller } from "../../components/Bestseller/BestSeller";
+import { ProductCard } from "../../components/ProductCard/ProductCard";
 import "./Homepage.css";
 
 export const Homepage = () => {
@@ -66,8 +67,98 @@ export const Homepage = () => {
             </div>
           </div>
           <h6 className="more-categories child-1">Wooden toys</h6>
+          <div className="product-container">
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+          </div>
           <h6 className="more-categories child-2">Stuffed toys</h6>
+          <div className="product-container">
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+          </div>
           <h6 className="more-categories child-3">Logic toys</h6>
+          <div className="product-container">
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+            <div className="product-item">
+              <ProductCard
+                productImg="http://fastortheme.com/toys/image/cache/catalog/demo/sony_vaio_1-200x200.jpg"
+                productName="Sony Vaio"
+                productPrice="$100"
+              />
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
